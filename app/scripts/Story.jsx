38346d4c2cee7886d8df58298ca45b5e85e0f114
Thijs Wouters <thijs@morewood.be>
2015-04-09 00:15:32 +0200

    Add full story.

--- app/scripts/Story.jsx
+++ app/scripts/Story.jsx
@@ -2,9 +2,27 @@ var Story = React.createClass({
   render: function () {
     return (
       <div>
-        <h1>Story</h1>
-        <p>Dit verhaal gaat over de korte <HiddenWord>ei</HiddenWord> en de lange ij.</p>
-        <p>Zo kan iemand leren of een woord met een lange ij, dan wel met een korte <HiddenWord>ei</HiddenWord> is.</p>
+        <h1>De <HiddenWord>keizerin</HiddenWord> gaat op <HiddenWord>reis</HiddenWord></h1>
+
+        <p>Toen de <HiddenWord>keizerin</HiddenWord> een <HiddenWord>klein</HiddenWord> <HiddenWord>meisje</HiddenWord> was, ging ze graag <HiddenWord>zeilen</HiddenWord>. Soms ging ze in het <HiddenWord>geheim</HiddenWord> naar de <HiddenWord>heide</HiddenWord>. Na een <HiddenWord>eindje</HiddenWord> wandelen langs een <HiddenWord>wei</HiddenWord>, waar <HiddenWord>geiten</HiddenWord> stonden, kwam ze aan een <HiddenWord>steiger</HiddenWord>. Met een <HiddenWord>eigen</HiddenWord> <HiddenWord>zeilbootje</HiddenWord> <HiddenWord>bereikte</HiddenWord> ze een <HiddenWord>eiland</HiddenWord>. Ze leek wel een echte <HiddenWord>kapitein</HiddenWord>.</p>
+
+        <p>Later <HiddenWord>reisde</HiddenWord> ze met de <HiddenWord>trein</HiddenWord>. Eerst nam ze nog <HiddenWord>afscheid</HiddenWord> van <HiddenWord>allerlei</HiddenWord> mensen: van <HiddenWord>allebei</HiddenWord> haar ouders, haar <HiddenWord>beide</HiddenWord> kinderen, haar <HiddenWord>meid</HiddenWord> en van haar man, de <HiddenWord>keizer</HiddenWord>, de <HiddenWord>leider</HiddenWord> en <HiddenWord>majesteit</HiddenWord> van het volk. Hij <HiddenWord>weigerde</HiddenWord> mee te gaan.</p>
+
+        <p>De <HiddenWord>scheiding</HiddenWord> was moeilijk.</p>
+
+        <p>Vanaf het <HiddenWord>plein</HiddenWord> voor het <HiddenWord>paleis</HiddenWord> reed de <HiddenWord>trein</HiddenWord> <HiddenWord>keihard</HiddenWord> weg. Als <HiddenWord>reiziger</HiddenWord> nam de <HiddenWord>keizerin</HiddenWord> <HiddenWord>weinig</HiddenWord> mee: een <HiddenWord>gebreide</HiddenWord> trui, soms wat gebakken <HiddenWord>eieren</HiddenWord> voor onderweg, een boterham met <HiddenWord>gelei</HiddenWord>, <HiddenWord>preisoep</HiddenWord> en steeds een bakje <HiddenWord>aardbeien</HiddenWord>. Dit was <HiddenWord>eigenlijk</HiddenWord> goed voor haar <HiddenWord>gezondheid</HiddenWord>.</p>
+
+        <p>De <HiddenWord>trein</HiddenWord> reed langs <HiddenWord>steile</HiddenWord> bergen, over <HiddenWord>veilige</HiddenWord> rails, langs <HiddenWord>terreinen</HiddenWord> bezaaid met <HiddenWord>keien</HiddenWord>, langs groene <HiddenWord>weiden</HiddenWord>, met hier en daar een prachtige <HiddenWord>eik</HiddenWord>.</p>
+
+        <p>Ze wuifde naar de <HiddenWord>arbeiders</HiddenWord> en bezocht de <HiddenWord>heilige</HiddenWord> plaatsen. Het was een <HiddenWord>feit</HiddenWord> dat ze een lieve vrouw was.</p>
+
+        <p>Aan het <HiddenWord>eind</HiddenWord> van de <HiddenWord>reis</HiddenWord> kocht ze voor haar kinderen een <HiddenWord>lei</HiddenWord> en popjes, gemaakt van <HiddenWord>klei</HiddenWord>. Voor haar ouders kocht ze een <HiddenWord>bedsprei</HiddenWord> en voor haar man lekkere <HiddenWord>marsepein</HiddenWord>. Toen kwam ze <HiddenWord>eindelijk</HiddenWord> terug.</p>
+
+        <p>De <HiddenWord>aanleiding</HiddenWord> voor haar terugkeer was een <HiddenWord>geheimzinnig</HiddenWord> bericht. Iemand had met een <HiddenWord>kei</HiddenWord> de mooie <HiddenWord>fontein</HiddenWord> geraakt, en die <HiddenWord>fontein</HiddenWord> was haar <HiddenWord>eigendom</HiddenWord>. Ze <HiddenWord>zei</HiddenWord>: "Ik <HiddenWord>eis</HiddenWord> een <HiddenWord>uitgereid</HiddenWord> onderzoek en ik ben <HiddenWord>bereid</HiddenWord> te zorgen voor de reparatie."</p>
+
+        <p>De mensen bewonderden haar <HiddenWord>kwaliteiten</HiddenWord>. Ze bezat de <HiddenWord>eigenschap</HiddenWord> dat ze in een ruzie een goede <HiddenWord>scheidsrechter</HiddenWord> was.</p>
+
+        <p>"Volgend <HiddenWord>seizoen</HiddenWord>, in <HiddenWord>mei</HiddenWord>," <HiddenWord>zei</HiddenWord> ze nog, "geef ik een <HiddenWord>seintje</HiddenWord> en maken we <HiddenWord>uiteindelijk</HiddenWord> samen een nieuwe <HiddenWord>reis</HiddenWord>."</p>
       </div>
     );
   }
